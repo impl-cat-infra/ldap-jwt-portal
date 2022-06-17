8286c9b37bdeba40133c82f1d8ef93064e0a16d3
Liu Xiaoyi <circuitcoder0@gmail.com>
2022-06-18 02:56:44 +0800

    Initial impl

---
+++ src/config.example.mjs
@@ -0,0 +1,7 @@
+export const PORT = 8734;
+export const JWT_KEY = "meow";
+export const JWT_COOKIE_NAME = "token";
+export const PORT_OFFSET = 10000;
+
+export const LDAP_REMOTE = ["ldaps://example.com"];
+export const LDAP_SCOPE = "ou=People,dc=example,dc=com";
---
+++ src/main.mjs
@@ -0,0 +1,71 @@
+import Koa from 'koa';
+import KoaBP from 'koa-bodyparser';
+import ldap from 'ldapjs';
+import jwt from 'jsonwebtoken';
+
+import * as CFG from './config.mjs';
+import fs from 'fs/promises';
+
+async function auth(user, pass) {
+  const conn = ldap.createClient({
+    url: CFG.LDAP_REMOTE,
+  });
+  await new Promise((resolve, reject) => conn.bind(`uid=${user},ou=People,dc=impl,dc=cat`, pass, (err) => {
+    if(err) reject(err);
+    else resolve();
+  }));
+
+  console.log(`Basic auth passed for ${user}`);
+
+  const uid = await new Promise((resolve, reject) => conn.search(`uid=${user},ou=People,dc=impl,dc=cat`, {
+    attributes: ['uidNumber'],
+  }, (err, res) => {
+    if(err) reject(err);
+    res.on('searchEntry', ent => {
+      resolve(ent.object.uidNumber);
+    });
+    res.on('error', (err) => {
+      console.error('error: ' + err.message);
+      reject(err);
+    });
+  }));
+
+  return parseInt(uid, 10);
+}
+
+const app = new Koa();
+
+app.use(KoaBP());
+
+const index = fs.readFile('index.html').then(buf => buf.toString('utf-8'));
+
+app.use(async ctx => {
+  if(ctx.method === 'GET') {
+    ctx.body = await index;
+    return ctx.status = 200;
+  } else if(ctx.method === 'POST') {
+    if((ctx.request.body?.user ?? '') === '') return ctx.status = 400;
+    if((ctx.request.body?.pass ?? '') === '') return ctx.status = 400;
+
+    try {
+      const uid = await auth(ctx.request.body.user, ctx.request.body.pass);
+      const port = uid + CFG.PORT_OFFSET;
+      // Oh no CPU burning
+      const token = jwt.sign({
+        user: ctx.request.body.user,
+        port,
+      }, CFG.JWT_KEY);
+
+      ctx.cookies.set(CFG.JWT_COOKIE_NAME, token);
+      ctx.body = { ok: true };
+      return ctx.status = 200;
+    } catch(e) {
+      console.error(e);
+      return ctx.status = 403;
+    }
+  } else {
+    ctx.status = 404;
+  }
+});
+
+app.listen(CFG.PORT);
